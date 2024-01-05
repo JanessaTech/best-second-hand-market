45484ab02eda01b8ab83d4f706499bb7d4faaee0
Janessa <lvlajzhao1983@gmail.com>
2024-01-05 18:47:52 +0800

    header layout

--- src/components/reusables/header/Header.js
+++ src/components/reusables/header/Header.js
@@ -1,6 +1,7 @@
-import { Box } from '@mui/material'
+import { Avatar, Box, Button, IconButton, InputAdornment, TextField, Typography } from '@mui/material'
 import React from 'react'
 import {headerHeight} from '../../../common/constant'
+import { CheapIcon } from '../../../utils/Svgs'
 
 export default function Header() {
   return (
@@ -8,7 +9,46 @@ export default function Header() {
         width: 1, height: headerHeight, 
         backgroundColor: 'black', 
         position: 'fixed', top: 0, left: 0, 
-        zIndex: (theme) => theme.zIndex.drawer + 1}}>      
+        px:3,
+        boxSizing:'border-box',
+        zIndex: (theme) => theme.zIndex.drawer + 1}}> 
+        <Box sx={{display: 'flex', alignItems: 'center', justifyContent:'space-between'}}>
+            <Box sx={{display: 'flex', alignItems: 'center'}}>
+                <IconButton href='#' sx={{pl:0}}>
+                    <Avatar alt='Cheap' src='/imgs/handshake.svg' sx={{ width:60, height:60}}/>
+                </IconButton>
+                <Typography variant='h4' color='white'>Cheap</Typography>
+            </Box>
+            <Box>
+                <Button variant='contained' sx={{textTransform:'none', fontSize:'1.2em'}}>Mint your own NFT</Button>
+            </Box>
+            <Box sx={{width:0.3}}>
+                <TextField sx={{backgroundColor:'white', borderRadius:3}}
+                    variant="outlined" 
+                    placeholder='Search'
+                    fullWidth
+                    InputProps={{
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <CheapIcon name={'search'}/>
+                            </InputAdornment>
+                        ),
+                        endAdornment:(
+                            <InputAdornment position="end">
+                                <IconButton sx={{ 
+                                    '&.MuiButtonBase-root':{
+                                        p:0.5, borderRadius:2, backgroundColor:'action.hover'}
+                                    }} disableRipple>
+                                    <CheapIcon name={'emap-line'} size={20}/>
+                                </IconButton>
+                            </InputAdornment>
+                        )
+                    }}
+                >
+                </TextField>
+            </Box>
+            <Box sx={{ width:300, height:76, backgroundColor:'yellow'}}></Box>
+        </Box>     
     </Box>
   )
 }
---
+++ src/utils/Svgs.js
@@ -0,0 +1,20 @@
+import { Icon } from "@mui/material"
+
+const iconSize = 30
+const svgMapping = new Map([
+  ['handshake', 'imgs/handshake.svg'],
+  ['search', 'imgs/search.svg'],
+  ['emap-line', 'imgs/emap-line.svg'],
+  
+
+])
+
+function generateIcon(src, size) {
+  return (
+    <Icon sx={{width:size, height:size}}>
+      <img src={src} height={size} width={size}/>
+    </Icon>
+  )
+}
+
+export function CheapIcon({name,size}) {return (generateIcon(svgMapping.get(name) || 'imgs/bookmarker.svg', size || iconSize))}
